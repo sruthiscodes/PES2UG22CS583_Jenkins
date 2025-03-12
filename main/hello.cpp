d7e6483f8f51ba04196851a26e933561157b758d
Sruthi <contactsruthis@gmail.com>
2025-03-12 22:09:42 +0530

    modify hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, World\n";
+  cout << "Hello, World, this is a change\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
   return 0;
